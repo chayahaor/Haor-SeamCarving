0674c804e2b753f8686900b94922d3026e306f25
chaya <chayaz.haor@gmail.com>
2022-12-13 21:28:52 -0500

    added seam class

---
+++ src/main/java/usingInts/Seam.java
@@ -0,0 +1,42 @@
+package usingInts;
+
+import java.util.ArrayList;
+
+public class Seam {
+
+    private final int widthPixels;
+    private final int heightPixels;
+    private int[][] startingEnergy;
+
+
+    public Seam(int[][] energyTable) {
+        widthPixels = energyTable[0].length;
+        heightPixels = energyTable.length;
+        startingEnergy = energyTable;
+    }
+
+    private ArrayList<Integer> findSmallestSeam() {
+        ArrayList<Integer> smallSeam = new ArrayList<>();
+        for (int row = 0; row < heightPixels; row++)
+        {
+            for (int col = 0; col < widthPixels; col++)
+            {
+                if (row == heightPixels - 1)
+                {
+
+                } else if (col == 0)
+                {
+
+                } else if (col == widthPixels - 1)
+                {
+
+                } else
+                {
+
+                }
+
+            }
+        }
+        return smallSeam;
+    }
+}
